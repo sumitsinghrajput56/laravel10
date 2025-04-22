3ccf5c73305cf55518369853634d98c08e1013cb
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-04-22 20:24:22 +0530

    make admin logout option 29

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +28,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::controller(AdminController::class)->group(function(){
+    Route::get('/admin/logout','destroy')->name('admin.logout');
+}); 
 
 require __DIR__.'/auth.php';

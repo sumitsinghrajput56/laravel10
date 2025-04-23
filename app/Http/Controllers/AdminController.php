d4ac9062e3b048a104c618e0de47f3d9f972f156
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-04-23 20:30:10 +0530

    added toaster message

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -54,7 +54,12 @@ class AdminController extends Controller
         }
         $data->save();
 
-        return redirect()->route('admin.profile');
+        $notification=array(
+            'message'=>'Admin Profile Updated Successfully',
+            'alert-type'=>'success'
+        );
+
+        return redirect()->route('admin.profile')->with($notification);
 
 
 
